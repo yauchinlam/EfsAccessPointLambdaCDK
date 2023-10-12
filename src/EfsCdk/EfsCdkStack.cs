544f20517a5ac1b628bc886d1c0fbe5ee33453f2
Yauchin M. Lam <yauchinlam17@gmail.com>
2023-10-11 23:34:16 -0400

    Initial commit

---
+++ src/EfsCdk/EfsCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace EfsCdk
+{
+    public class EfsCdkStack : Stack
+    {
+        internal EfsCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
