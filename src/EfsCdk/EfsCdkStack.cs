0b0ffb3cfbd9059e1d5a6467d96504866f78ff61
Yauchin M. Lam <yauchinlam17@gmail.com>
2023-10-12 00:01:19 -0400

    CDK with EFS, Lambda, and accesspoint

--- src/EfsCdk/EfsCdkStack.cs
+++ src/EfsCdk/EfsCdkStack.cs
@@ -1,13 +1,45 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.EFS;
+using Amazon.CDK.AWS.Lambda;
 using Constructs;
 
 namespace EfsCdk
 {
     public class EfsCdkStack : Stack
     {
-        internal EfsCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        internal EfsCdkStack(Constructs.Construct scope, 
+            string id, 
+            IStackProps props = null) : 
+            base(scope, id, props)
         {
-            // The code that defines your stack goes here
-        }
+            // The code that defines
+            // your stack goes here
+            var efs = new Amazon.CDK.AWS.EFS.FileSystem(
+                this,
+                "FirstEFS",
+                new FileSystemProps()
+                {
+                    LifecyclePolicy = LifecyclePolicy.AFTER_14_DAYS,
+                });
+
+            var accessPoint = new Amazon.CDK.AWS.EFS.AccessPoint(
+                this,
+                "FirstAccessPoint",
+                new AccessPointProps()
+                {
+                    FileSystem = efs,
+                });
+
+            var functionApp = new DockerImageFunction(this, "MyfirstDockerImageApp", new DockerImageFunctionProps()
+            {
+                Code = DockerImageCode.FromImageAsset("/src", new AssetImageCodeProps()
+                {
+                    File = "AWSLambda1/Dockerfile",
+                }),
+                Filesystem = Amazon.CDK.AWS.Lambda.FileSystem.
+                FromEfsAccessPoint(accessPoint, 
+                "/mnt/efs"),
+            }) ;
+		}
     }
 }
